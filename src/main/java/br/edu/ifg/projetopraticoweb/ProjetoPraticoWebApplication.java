5f8f15894ed7ae57d2f1f07080838e2e0b8ec189
jaypl89 <jarbasspl@gmail.com>
2024-09-09 22:38:37 -0300

    create initial configuration

---
+++ src/main/java/br/edu/ifg/projetopraticoweb/ProjetoPraticoWebApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifg.projetopraticoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPraticoWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoPraticoWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/ifg/projetopraticoweb/ProjetoPraticoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifg.projetopraticoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPraticoWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
